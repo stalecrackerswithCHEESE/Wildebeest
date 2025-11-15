aa1c79be7ff260bc9265322e3e13e47ecd7530fd
dont forget that cheese! <114269866+stalecrackerswithCHEESE@users.noreply.github.com>
2025-11-15 10:08:38 -0700

    Update TeleportHandler.lua

--- ServerScriptService/TeleportHandler.lua
+++ ServerScriptService/TeleportHandler.lua
@@ -8,8 +8,23 @@ local zones = workspace.TeleportZones
 local events = ReplicatedStorage.Events
 local zoneEvent = events.ZoneEvent
 
--- Table of zones and their properties
-local teleportZones = {
+-- Default zone
+local defaultZone = {
+	["owner"] = nil,
+	["max"] = 1,
+	["current"] = 0,
+	["players"] = {},
+	["created"] = false
+}
+
+-- Change the player count interface for the teleport zone
+local function updatePlayerCount(zoneId, current, max)
+	game.Workspace.TeleportZones[zoneId].BillboardGui.PlayerCount.Text = current .. "/" .. max
+end
+
+local teleportZones = {}
+
+teleportZones = {
 	[1] = {
 		["owner"] = nil,
 		["max"] = 1,
@@ -61,18 +76,17 @@ local teleportZones = {
 	}
 }
 
+local function canTouch(i, val)
+	for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
+		b.CanTouch = val
+	end
+end
+
 -- Intialize the teleport zones
 for i, v in zones:GetChildren() do
 	if not v:IsA("Model") then continue end
 
-	for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-		b.CanTouch = true
-	end
-end
-
--- Change the player count interface for the teleport zone
-local function updatePlayerCount(zoneId, current, max)
-	game.Workspace.TeleportZones[zoneId].BillboardGui.PlayerCount.Text = current .. "/" .. max
+	canTouch(i, true)
 end
 
 -- Go through everything inside the zones
@@ -81,86 +95,85 @@ for i, v in zones:GetChildren() do
 	for k, p in v.Border:GetChildren() do
 		
 		p.Touched:Connect(function(hit)
-			if hit.Parent then
-				-- Try to find a Humanoid in the parent
+			if hit.Parent and hit.Parent:FindFirstChildWhichIsA("Humanoid") and game.Players:GetPlayerFromCharacter(hit.Parent) then
+
+				-- If a Humanoid is found, it's a character					
 				local humanoid = hit.Parent:FindFirstChildWhichIsA("Humanoid")
+				
+				-- Get the player from the character's model
+				local player = Players:GetPlayerFromCharacter(hit.Parent)						
+				
+				-- Checks if player isn't in the zone
+				if not table.find(teleportZones[i].players, player.UserId) then
+					
+					-- If so, add the player to the list of players in that zone
+					table.insert(teleportZones[i].players, player.UserId)
 
-				-- If a Humanoid is found, it's a character
-				if humanoid then
-					-- Get the player from the character's model
-					local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-					if player then
-						
-	
-						local zoneNumber = i
-						
-						-- Checks if player isn't in the zone
-						if not table.find(teleportZones[i].players, player.UserId) then
-							
-							table.insert(teleportZones[i].players, player.UserId)
-
-							player.Character.HumanoidRootPart.CFrame = game.Workspace.TeleportZones[i].TP.CFrame
-							game.Workspace.TeleportZones[i].BillboardGui.StateLabel.Text = "Creating party..."
-							
-							-- Makes all of the parts in the teleport zones uncollidable
-							for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-								b.CanTouch = false
-							end
-						else
-							return
-						end
-						
-						-- Checks if there is an owner to the zone
-						if (teleportZones[i].owner == nil) then
-							teleportZones[i].owner = player.UserId		
-							teleportZones[i].current = 1
-							print("Current:" .. teleportZones[i].current)
-
-							zoneEvent:FireClient(player, "showgui")
-
-						else
-							teleportZones[i].current += 1
-							print("Current:" .. teleportZones[i].current)
-							zoneEvent:FireClient(player, "shownothostgui")
-						end
-						
-						updatePlayerCount(i,teleportZones[i].current, teleportZones[i].max)	
-						
-						-- Checks if a party is already created
-						if teleportZones[i].created then
-							
-							-- Check if full
-							if teleportZones[i].current == teleportZones[i].max then
-								
-								-- Makes it so you cant join
-								for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-									b.CanTouch = false
-
-								end
-
-							else
-								
-								-- Allow people to join
-								for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-									b.CanTouch = true
-								end
-
-							end
-							
-						end
-																		
-					end
+					-- Teleports the player into the zone
+					player.Character.HumanoidRootPart.CFrame = game.Workspace.TeleportZones[i].TP.CFrame
+					game.Workspace.TeleportZones[i].BillboardGui.StateLabel.Text = "Creating party..."
+					
+					-- Makes all of the parts in the teleport zones uncollidable
+					canTouch(i, false)
+				else
+					
+					-- The player is in the zone, they cant exit. They have to click the leave button
+					return
 				end
+				
+				-- Checks if there is an owner to the zone
+				if (teleportZones[i].owner == nil) then
+					
+					-- There is not an owner, make that player the new owner
+					
+					-- Set them as owner
+					teleportZones[i].owner = player.UserId		
+					teleportZones[i].current = 1
+					
+					-- Show them the "owner" gui
+					zoneEvent:FireClient(player, "showgui")
+
+				else
+					
+					-- They are not the owner, show them the normal gui
+					
+					teleportZones[i].current += 1
+					
+					zoneEvent:FireClient(player, "shownothostgui")
+				end
+				
+				
+				-- Update the player count on the billboard gui
+				updatePlayerCount(i,teleportZones[i].current, teleportZones[i].max)	
+				
+				-- Checks if a party is already created
+				if teleportZones[i].created then
+					
+					-- Check if max players are in the zone
+					if teleportZones[i].current == teleportZones[i].max then
+						
+						-- If so, makes it so others cant join
+						canTouch(i, false)
+
+					else
+						
+						-- Otherwise, Allow other players to join
+						canTouch(i, true)
+					end
+					
+				end
+																
 			end
+			
 		end)
-		
 	end
 end
 
 -- Deletes party and kicks everyone out
 local function disband(i)
 	
-	print("Disband: "..i)
+
+	-- Resets the zone
 	
 	teleportZones[i].owner = nil
 	teleportZones[i].current = 0
@@ -168,19 +181,15 @@ local function disband(i)
 	teleportZones[i].max = 1
 
 	-- Make all borders interactable
-	for e, v in zones:GetChildren() do
-		if not v:IsA("Model") then continue end
 
-		for a, b in game.Workspace.TeleportZones[e].Border:GetChildren() do
-			b.CanTouch = true
-		end
-	end
+	canTouch(i, true)
 
 
 	-- Set players to 0/1
 	updatePlayerCount(i, 0, 1)
 
 
+	-- Reset message
 	game.Workspace.TeleportZones[i].BillboardGui.StateLabel.Text = "Waiting for players..."
 
 end
@@ -188,42 +197,34 @@ end
 -- When zoneEvent is fired
 zoneEvent.OnServerEvent:Connect(function(playerFired, type, amount)
 	
-	-- Player leaves
+	-- Player presses leave
 	if type == "leave" then
-
+		
+		-- Find which zone the player is in
 		for i, q in teleportZones do
 			if not table.find(q.players, playerFired.UserId) then continue end
 			
+			-- If they are the owner, set the owner to 0
 			if teleportZones[i].owner == playerFired.UserId then
 				teleportZones[i].owner = 0
 			end
 
+			-- Teleport the player to the disband part
 			playerFired.Character.HumanoidRootPart.CFrame = game.Workspace.Disband.CFrame
 
+			-- Remove them from the list of users
 			table.remove(teleportZones[i].players, table.find(teleportZones[i].players, playerFired.UserId))
 			
+			-- Subtract 1 from the current player count
 			teleportZones[i].current -= 1
-			print("Current: ", teleportZones[i].current)
-
+			
+			-- Update the gui with the player count
 			updatePlayerCount(i, teleportZones[i].current, teleportZones[i].max)
 			
-			-- Checks if maximum players
-			if teleportZones[i].current == teleportZones[i].max then
-
-				for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-					b.CanTouch = false
-
-				end
-
-			else
-
-				for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-					b.CanTouch = true
-				end
-
-			end
+			-- Allow players to join if not max
+			canTouch(i, not (teleportZones[i].current == teleportZones[i].max))
 			
-			-- Checks if players are in the zone
+			-- If there are no players, disband
 			if teleportZones[i].current == 0 then
 				teleportZones[i].created = false
 				disband(i)
@@ -233,40 +234,33 @@ zoneEvent.OnServerEvent:Connect(function(playerFired, type, amount)
 
 		end
 
-	-- Player creates party
+	-- Player presses create
 	elseif type == "create" then
 			
 		
 		for i, q in teleportZones do
+			
+			-- Find player
 			
 			if q.owner ~= playerFired.UserId then continue end
 		
 			teleportZones[i].max = amount
 			teleportZones[i].created = true
 			
-			-- Check if max players
-			if teleportZones[i].current == teleportZones[i].max then
-
-				for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-					b.CanTouch = false
-
-				end
+			-- Allow players to join if not max
+			canTouch(i, not (teleportZones[i].current == teleportZones[i].max))
 			
-			else
-				
-				for a, b in game.Workspace.TeleportZones[i].Border:GetChildren() do
-					b.CanTouch = true
-				end
-
-			end
-			
+			-- Update the gui with the player countt
 			updatePlayerCount(i, 1, teleportZones[i].max)
 			
 			-- Shows a countdown from 20 seconds to 1
 			for t=20, 1, -1 do
+				-- If anyone leaves (i.e. not created), then cancel
 				if not teleportZones[i].created then
 					return
 				end				
+				
+				-- Display how many seconds left until teleport
 				workspace.TeleportZones[i].BillboardGui.StateLabel.Text = "Leaving in " .. tostring(t) .. "..."
 				task.wait(1)
 				
@@ -285,25 +279,30 @@ zoneEvent.OnServerEvent:Connect(function(playerFired, type, amount)
 			local teleportOptions = Instance.new("TeleportOptions")
 			teleportOptions.ShouldReserveServer = true
 			
+			-- Count of players for the game to know how many
 			local teleportData = {
 				["players"] =  #teleportZones[i].players
 			}
 			
-			
 			-- Set options and teleport
 			teleportOptions:SetTeleportData(teleportData)
+			
+			-- Try to teleport
 			local success, result = pcall(function()
 				TeleportService:TeleportAsync(placeId, playerList, teleportOptions)
 			end)
 			
+			-- Checks if the the teleport was a success, and if not warns that it failed
 			if success then
 			else
 				warn("Teleport failed: " .. tostring(result))
 			end
 			
+			-- Delay for teleport
 			task.wait(1)
 			workspace.TeleportZones[i].BillboardGui.StateLabel.Text = "Teleporting..."
 			
+			-- Disband and reset zone
 			teleportZones[i].created = false
 			disband()			
 		end
